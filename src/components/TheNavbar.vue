524772b3dc67ebd7f817347f668f0e288c6745d2
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-08-29 00:42:43 +0200

    Navbar design

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="flex justify-between align-middle p-3 mt-2">
+    <div>
+      <p class="text-gray-100 font-semibold">Mostakimul Karim</p>
+      <p class="text-gray-100 font-sans font-extralight">
+        Email: mostakimulkarim@gmail.com
+      </p>
+    </div>
+    <button class="self-center px-4 py-1 bg-gray-100 font-semibold">
+      Logout
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-    Chatroom vue
+  <div class="bg-pink-700 w-3/4 mx-auto rounded-md">
+    <TheNavbar />
   </div>
 </template>
 
 <script>
+import TheNavbar from '@/components/TheNavbar.vue';
 export default {
+  components: {
+    TheNavbar,
+  },
   setup() {
     return {};
   },
